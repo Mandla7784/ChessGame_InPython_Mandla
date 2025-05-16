b77e51473918b472619ce5ca88cdb5ff6c96a410
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-16 13:20:52 +0200

    Added assests

--- GamePieces.py
+++ GamePieces.py
@@ -19,32 +19,32 @@ black_knight_small = pygame.transform.scale(black_knight, (45, 45))
 black_pawn = pygame.image.load('./images/black pawn.png')
 black_pawn = pygame.transform.scale(black_pawn, (65, 65))
 black_pawn_small = pygame.transform.scale(black_pawn, (45, 45))
-white_queen = pygame.image.load('./images/white queen.png')
-white_queen = pygame.transform.scale(white_queen, (80, 80))
-white_queen_small = pygame.transform.scale(white_queen, (45, 45))
-white_king = pygame.image.load('./images/white king.png')
-white_king = pygame.transform.scale(white_king, (80, 80))
-white_king_small = pygame.transform.scale(white_king, (45, 45))
-white_rook = pygame.image.load('./images/white rook.png')
-white_rook = pygame.transform.scale(white_rook, (80, 80))
-white_rook_small = pygame.transform.scale(white_rook, (45, 45))
-white_bishop = pygame.image.load('./images/white bishop.png')
-white_bishop = pygame.transform.scale(white_bishop, (80, 80))
-white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
-white_knight = pygame.image.load('./images/white knight.png')
-white_knight = pygame.transform.scale(white_knight, (80, 80))
-white_knight_small = pygame.transform.scale(white_knight, (45, 45))
-white_pawn = pygame.image.load('./images/white pawn.png')
-white_pawn = pygame.transform.scale(white_pawn, (65, 65))
-white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
+# white_queen = pygame.image.load('./images/white queen.png')
+# white_queen = pygame.transform.scale(white_queen, (80, 80))
+# white_queen_small = pygame.transform.scale(white_queen, (45, 45))
+# white_king = pygame.image.load('./images/white king.png')
+# white_king = pygame.transform.scale(white_king, (80, 80))
+# white_king_small = pygame.transform.scale(white_king, (45, 45))
+# white_rook = pygame.image.load('./images/white rook.png')
+# white_rook = pygame.transform.scale(white_rook, (80, 80))
+# white_rook_small = pygame.transform.scale(white_rook, (45, 45))
+# white_bishop = pygame.image.load('./images/white bishop.png')
+# white_bishop = pygame.transform.scale(white_bishop, (80, 80))
+# white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
+# white_knight = pygame.image.load('./images/white knight.png')
+# white_knight = pygame.transform.scale(white_knight, (80, 80))
+# white_knight_small = pygame.transform.scale(white_knight, (45, 45))
+# white_pawn = pygame.image.load('./images/white pawn.png')
+# white_pawn = pygame.transform.scale(white_pawn, (65, 65))
+# white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
 
-white_images = [white_pawn, white_queen, white_king,
+# white_images = [white_pawn, white_queen, white_king,
 
-                white_knight, white_rook, white_bishop]
+#                 white_knight, white_rook, white_bishop]
 
-small_white_images = [white_pawn_small, white_queen_small, white_king_small, white_knight_small,
+# small_white_images = [white_pawn_small, white_queen_small, white_king_small, white_knight_small,
 
-                      white_rook_small, white_bishop_small]
+#                       white_rook_small, white_bishop_small]
 
 black_images = [black_pawn, black_queen, black_king,
 
@@ -56,6 +56,26 @@ small_black_images = [black_pawn_small, black_queen_small, black_king_small, bla
 
 piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
 
+#variables and images
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
+
+captured_pieces_white = []
+captured_pieces_black = []
 
 
 
--- GameScreen.py
+++ GameScreen.py
@@ -1,36 +1,91 @@
 import pygame
+import GamePieces
 
-
-#Screen
+# Screen
 class Screen:
     def __init__(self):
-        pass
+        pygame.init()
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
+        self.medium_font = pygame.font.Font('freesansbold.ttf', 40)
+        self.big_font = pygame.font.Font('freesansbold.ttf', 50)
+        self.timer = pygame.time.Clock()
+        self.fps = 60
+        self.screen = None
 
-
-
-    def viewScreen( width ,height):
-        screen = pygame.display.set_mode([width  , height])
+    def viewScreen(self, width, height):
+        self.screen = pygame.display.set_mode([width, height])
         pygame.display.set_caption("Two player chess game")
-        font = pygame.font.Font('freesansbold.ttf', 20)
-        medium_font = pygame.font.Font('freesansbold.ttf', 40)
-        big_font = pygame.font.Font('freesansbold.ttf', 50)
-        timer = pygame.time.Clock()
-        fps = 60
-        
-    def draw_board():
+
+    def draw_Pieces_into_board(self):
+        for i in range(len(GamePieces.white_pieces)):
+            index = GamePieces.piece_list.index(GamePieces.white_pieces[i])
+            if GamePieces.white_pieces[i] == "pawn":
+                self.screen.blit(
+                    GamePieces.white_pawn,
+                    (
+                        GamePieces.white_locations[i][0] * 100 + 22,
+                        GamePieces.white_locations[i][1] * 100 + 22
+                    )
+                )
+            else:
+                self.screen.blit(
+                    GamePieces.white_images[index],
+                    (
+                        GamePieces.white_locations[i][0] * 100 + 10,
+                        GamePieces.white_locations[i][1] * 100 + 10
+                    )
+                )
+
+            if GamePieces.turn_step < 2 and GamePieces.selection == i:
+                pygame.draw.rect(
+                    self.screen,
+                    'red',
+                    [
+                        GamePieces.white_locations[i][0] * 100 + 1,
+                        GamePieces.white_locations[i][1] * 100 + 1,
+                        100, 100
+                    ],
+                    2
+                )
+
+    def draw_board(self, width, height):
         for i in range(32):
             column = i % 4
-            row = i  // 4
-            
-            if row  % 2 == 0:
-                pygame.draw.rect(self , "light gray" , [
-                    
-                    600 - (column * 200), row * 100, 100, 100
-                    
-                ])
+            row = i // 4
+
+            if row % 2 == 0:
+                pygame.draw.rect(
+                    self.screen,
+                    "light gray",
+                    [600 - (column * 200), row * 100, 100, 100]
+                )
             else:
-                 pygame.draw.rect(self, 'light gray', [
+                pygame.draw.rect(
+                    self.screen,
+                    'light gray',
+                    [700 - (column * 200), row * 100, 100, 100]
+                )
 
-                             700 - (column * 200), row * 100, 100, 100])
+        pygame.draw.rect(self.screen, "gray", [0, 800, width, 100])
+        pygame.draw.rect(self.screen, 'gray', [0, 800, width, 100])
+        pygame.draw.rect(self.screen, 'gold', [0, 800, width, 100], 5)
+        pygame.draw.rect(self.screen, 'gold', [800, 0, 200, height], 5)
 
-        
+        status_text = [
+            'White: Select a Piece to Move!',
+            'White: Select a Destination!',
+            'Black: Select a Piece to Move!',
+            'Black: Select a Destination!'
+        ]
+
+        self.screen.blit(
+            self.big_font.render(
+                status_text[GamePieces.turn_step], True, 'black'
+            ),
+            (20, 820)
+        )
+
+
+if __name__ == "__main__":
+    screen = Screen()
+    screen.viewScreen(1000, 900)
