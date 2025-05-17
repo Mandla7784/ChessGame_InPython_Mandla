e471b0e713f17d5085c80943f5c6e6012a24c089
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-17 20:20:36 +0200

    Added piece abstract class

--- package/GamePieces.py
+++ package/GamePieces.py
@@ -1,9 +1,34 @@
 
 class Piece:
-    def __init__(self):
+    def __init__(self,color):
+        self.color = color
+        
+    def set_moves(self, pos , board):
         pass
+         
+    def getMoves(self , position , board):
+        #logic for moves
+        
+        return []
+      
+      
+#creating a Pawn pievc extending the abstract class 
+
+class Pawn(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        
     
+
+class Knight(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        
     
+
+      
+
+ 
     
 if  __name__== "__main__":
     
