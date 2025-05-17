2d99696df252861580452a699a86dbe76a1afe97
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-17 20:14:05 +0200

    create a board diplaying it on console

--- package/main.py
+++ package/main.py
@@ -2,8 +2,29 @@ import pygame
 pygame.init()
 
 
+def create_board():
+    board = [
+      ["r", "n", "b", "q", "k", "b", "n", "r"],  # Black pieces
+        ["p"] * 8,
+        [" "] * 8,
+        [" "] * 8,
+        [" "] * 8,
+        [" "] * 8,
+        ["P"] * 8,
+        ["R", "N", "B", "Q", "K", "B", "N", "R"],  # White pieces  
+    ]
+    
+    return board
+
+def print_board(board):
+
+    for row in board: # looping on each row and print the data
+        print(" ".join(row))
+        
+      
 def main():
-    pass
+    board = create_board()
+    print_board(board=board)
 
     
     
