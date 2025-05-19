40d291171f1f90011facebc1441b547625b19f7e
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-19 10:49:26 +0200

    Added is valid function on Piece class

--- package/GamePieces.py
+++ package/GamePieces.py
@@ -1,9 +1,36 @@
 
 class Piece:
-    def __init__(self,color):
+    def __init__(self,color , x ,y):
         self.color = color 
-    def set_moves(self, pos , board):
-        pass
+        self.x = x
+        self.y = y
+        
+    def __repr__(self):
+        return f"{self.color} {self.__class__.__name__} at ({self.x} , {self.y} )"
+    
+    
+    #check valid move on a piece 
+    def is_valid_move(self , new_x , new_y  ,board):
+        
+        if self.color == "white":
+            if new_x == self.x and new_y == self.y+1 and board[new_x][new_y] is None:
+                return True
+            
+    
+        if not (0 <= new_x < len(board) and 0 <= new_y < len(board)):
+            return False
+        return True
+    
+        
+     #setting a new position of a piece on board   
+    def set_moves(self, new_x, new_y , board):
+        
+        if(new_x > 0 and new_y > 0):
+            self.x = new_x
+            self.y = new_y
+        else:
+            return not self.is_valid_move(new_x , new_y , board)
+            
          
     def getMoves(self , position , board):
         #logic for moves
@@ -15,16 +42,9 @@ class Piece:
       
 #creating a Pawn pievc extending the abstract class 
 class Pawn(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    def __init__(self, color , x, y):
+        super().__init__(color ,x ,y)
         
-class Knight(Piece):
-    def __init__(self, color):
-        super().__init__(color)
-    
-class Queen(Piece):
-    def __init__(self, color):
-        super().__init__(color)
        
 
 if  __name__== "__main__":
