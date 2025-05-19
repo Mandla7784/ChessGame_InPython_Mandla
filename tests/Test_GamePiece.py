8528cab99321aa3fb9c659c03f937b5b884a4325
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-19 11:26:05 +0200

    Added test game piece

---
+++ tests/Test_GamePiece.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestGamePiece(unittest.TestCase):
+    def __init__(self, methodName = "runTest"):
+        super().__init__(methodName)
+        
+        
+
+
+
+
+
+if __name__=="__main__":
+    unittest.main()
