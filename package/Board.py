63bf9086fa3363db17a5ab8b08d6144212ff5714
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-24 16:28:41 +0200

    worked on some bugs

--- package/Board.py
+++ package/Board.py
@@ -10,13 +10,9 @@ Retain the square color pattern when empty.
 
 
 """
-
 class Board:
-
-    # Create the board and display it
-    def create_board(self):
-        
-            
+    @staticmethod
+    def create_board() -> list[list[str]]:
         columns = '  A  B  C  D  E  F  G  H'
         board = [
     ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
@@ -29,15 +25,14 @@ class Board:
     ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
 ]
 
-        
-        
+
         print(columns)
         for row in range(8):
             line = f"{8 - row} "
             for col in range(8):
-                piece = self.board[row][col]
+                piece = board[row][col]
                 square_color = "⬛" if (row + col) % 2 == 0 else "⬜"
                 line += f"{piece if piece != ' ' else square_color} "
             print(line)
         print(columns)
-        return  board  # RETURN the board so it's usable in game_loop()
+        return board
