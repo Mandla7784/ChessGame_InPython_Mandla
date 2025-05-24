26b4ab7083fc36bced466ef70f3aad0bd461d654
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-24 16:47:12 +0200

    test added

--- tests/Test_main.py
+++ tests/Test_main.py
@@ -5,8 +5,7 @@ from main import  create_board
 class Test_Main(unittest.TestCase):
     def __init__(self, methodName = "runTest"):
         super().__init__(methodName)
-        
-        
+       
     def test_create_board(self):
         self.assertEqual( create_board(),
   """
