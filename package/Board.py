f1bb7a5fb260d6399eb082dc349abf4057287da6
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-05-24 16:25:04 +0200

    Added static method

--- package/Board.py
+++ package/Board.py
@@ -12,33 +12,32 @@ Retain the square color pattern when empty.
 """
 
 class Board:
-    def __init__(self):
-        self.columns = '  A  B  C  D  E  F  G  H'
-        self.board = [
-            ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],  # 8
-            ["♟"] * 8,                                # 7
-            [" "] * 8,                                # 6
-            [" "] * 8,                                # 5
-            [" "] * 8,                                # 4
-            [" "] * 8,                                # 3
-            ["♙"] * 8,                                # 2
-            ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]   # 1
-        ]
-    
 
-        # Create the board and display it
-    def create_board():
-      
+    # Create the board and display it
+    def create_board(self):
+        
+            
+        columns = '  A  B  C  D  E  F  G  H'
+        board = [
+    ["♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"],
+    ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    ["♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+    ["♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"]
+]
+
+        
+        
+        print(columns)
         for row in range(8):
             line = f"{8 - row} "
             for col in range(8):
-                piece = board[row][col]
+                piece = self.board[row][col]
                 square_color = "⬛" if (row + col) % 2 == 0 else "⬜"
-                if piece != " ":
-                    line += piece + " "
-                else:
-                    line += square_color + " "
+                line += f"{piece if piece != ' ' else square_color} "
             print(line)
-
-
-        return board  # RETURN the board so it's usable in game_loop()
+        print(columns)
+        return  board  # RETURN the board so it's usable in game_loop()
